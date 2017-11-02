321497ac034b2876dbdfbe4af78ac753e070ae19
chandrasekharu <ucsraju@gmail.com>
2017-11-02 12:55:46 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Does this change?';
+  name='';
+  serverId: number= 12;
+  serverStatus: string='offline';
+  allowNewServer= false;
+
+  constructor() {
+    setTimeout( () =>{
+      this.allowNewServer=true;
+    },2000)
+  }
+
+  
+  getServerStatus(){
+    return this.serverStatus;
+  }
 }
---
+++ src/app/success/success.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  templateUrl: './success.component.html',
+  styles: [`
+  p{
+    padding: 20px;
+    background-color: palegreen;
+    border: 1px solid green;
+  } 
+  `]
+})
+export class SuccessComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
