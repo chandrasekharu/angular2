1d705315f2341850d76a26811e4a84041c1b3033
chandrasekharu <ucsraju@gmail.com>
2017-11-07 12:20:56 -0600

    built in directives ngStyle,ngClass, ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+  .online{
+    color:white;
+  }
+  .offline{
+    color:white;
+  }
+  `]
 })
 export class AppComponent {
   title = 'Does this change?';
@@ -13,13 +20,20 @@ export class AppComponent {
   allowNewServer= false;
   serverCreationStatus="No Server was created";
   serverName='';
+  servers=["server1","server2"];
 
   constructor() {
-    // setTimeout( () =>{
-    //   this.allowNewServer=true;
-    // },2000)
-
+    setTimeout( () =>{
+      this.allowNewServer=true;
+    },2000)
+    this.serverStatus= Math.random() > 0.5? 'online':'offline';
     
+  }
+  getFontColor(){
+    return this.serverStatus;
+  }
+  getColor(){
+    return this.serverStatus=== 'online'? 'green':'red';
   }
 
   onUpdateServerName(){
@@ -30,10 +44,11 @@ export class AppComponent {
     return this.serverStatus;
   }
   onCreateServer(){
+
+    this.allowNewServer=true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus="server was created with"+this.serverName;
   }
 
-  getColor(){
-    return this.serverStatus
-  }
+  
 }
